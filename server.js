fcbb704070c69abbf0ee33119d299bc675fc5bb6
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-24 20:34:34 -0700

    completed utils

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const helpers = require('./utils/helpers');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.engine({ helpers });
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
@@ -22,11 +22,11 @@ const sess = {
 };
 
 app.use(session(sess));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
