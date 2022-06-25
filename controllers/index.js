a481a2916b127406bac3b0500cf45fdc0800d3c6
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-24 21:41:14 -0700

    completed user-route

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./homepage-routes.js");
+const dashBoardRoutes = require("./dashboard-routes.js");
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/dashboard", dashBoardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
