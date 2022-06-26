c65403dc895e7667a678d10b3fa06cfc1f438bb7
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-06-26 12:57:08 -0700

    debugging

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const sessAuth = require('../utils/auth');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        where: {
+            // use the ID from the session
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }) 
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/edit/:id', sessAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_text', 'title', 'created_at'],
+        include: [
+            {
+                model: User, 
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User, 
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const post = dbPostData.get({ plain: true });
+        res.render('edit-posts', {post, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/newpost', (req, res) => {
+    res.render('new-posts');
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homepage-routes.js");
-// const dashBoardRoutes = require("./dashboard-routes.js");
+const dashBoardRoutes = require("./dashboard-routes.js");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-// router.use("/dashboard", dashBoardRoutes);
+router.use("/dashboard", dashBoardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
